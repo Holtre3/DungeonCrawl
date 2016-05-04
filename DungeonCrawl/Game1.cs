2f325e456aa9e82a88ef0726644df8f2c8b82ef9
Jason Holtrey <jasonholtrey@gmail.com>
2016-05-04 00:51:42 -0400

    Basic sprite and movement implementation.

--- DungeonCrawl/Game1.cs
+++ DungeonCrawl/Game1.cs
@@ -8,7 +8,8 @@ namespace DungeonCrawl
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        Texture2D eye;
+        Vector2 eyePos;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -16,12 +17,15 @@ namespace DungeonCrawl
         }
         protected override void Initialize()
         {
+            eyePos = new Vector2(0.0f, 0.0f);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            eye = Content.Load<Texture2D>("eye");
+            
         }
 
         protected override void UnloadContent()
@@ -33,17 +37,36 @@ namespace DungeonCrawl
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            KeyboardState keyboardState = Keyboard.GetState();
+            float time = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float speed = 100.0f;
+            float moveAmount = time * speed;
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                eyePos.Y += moveAmount;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                eyePos.Y -= moveAmount;
+            }
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                eyePos.X -= moveAmount;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                eyePos.X += moveAmount;
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-
-
+            GraphicsDevice.Clear(Color.White);
+            spriteBatch.Begin();
+            spriteBatch.Draw(eye, eyePos, Color.White);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
